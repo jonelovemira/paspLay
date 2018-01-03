03c0726a3a6272473c80bb5e655a62999a53b4fc
liuqiao <liuqiao@189.cn>
2018-01-03 17:30:36 +0800

    增加demo

--- demo/components/monitor/welcome/index.vue
+++ demo/components/monitor/welcome/index.vue
@@ -1,13 +1,128 @@
 <template>
-<div>  
-  <h3>线形进度条 — 百分比外显</h3>
-  <div style="width:30%">
-  <el-progress :percentage="0"></el-progress>
-  <el-progress :percentage="70"></el-progress>
-  <el-progress :percentage="100" status="success"></el-progress>
-  <el-progress :percentage="50" status="exception"></el-progress>
+<div> 
+  <h3>小红点</h3>
+  <p>出现在按钮、图标旁的数字或状态标记。</p>
+  <div>
+    <el-badge :value="12" class="item">
+      <el-button size="small">评论</el-button>
+    </el-badge>
+    <el-badge :value="3" class="item">
+      <el-button size="small">回复</el-button>
+    </el-badge>
+
+    <el-dropdown trigger="click">
+      <span class="el-dropdown-link">
+        点我查看<i class="el-icon-caret-bottom el-icon--right"></i>
+      </span>
+      <el-dropdown-menu slot="dropdown">
+        <el-dropdown-item class="clearfix">
+          评论
+          <el-badge class="mark" :value="12" />
+        </el-dropdown-item>
+        <el-dropdown-item class="clearfix">
+          回复
+          <el-badge class="mark" :value="3" />
+        </el-dropdown-item>
+      </el-dropdown-menu>
+    </el-dropdown>
+
   </div>
-  <h3 id="duo-xuan">多选</h3>
+
+  <h3>Alert 警告</h3>
+  <p>用于页面中展示重要的提示信息。</p>
+  <div class="demo-alert">
+    <el-alert
+        title="成功提示的文案"
+        type="success">
+      </el-alert>
+      <el-alert
+        title="消息提示的文案"
+        type="info">
+      </el-alert>
+      <el-alert
+        title="警告提示的文案"
+        type="warning">
+      </el-alert>
+      <el-alert
+        title="错误提示的文案"
+        type="error">
+      </el-alert>
+  </div>  
+
+  <h3>Notification通知</h3>
+  <p>带有 icon，常用来显示「成功、警告、消息、错误」类的系统消息</p>
+  <el-button
+    plain
+    @click="open3">
+    成功
+  </el-button>
+  <el-button
+    plain
+    @click="open4">
+    警告
+  </el-button>
+  <el-button
+    plain
+    @click="open5">
+    消息
+  </el-button>
+  <el-button
+    plain
+    @click="open6">
+    错误
+  </el-button>
+
+  <h3>Popover弹出框</h3>
+  <p>基础用法</p>
+  <div>
+    <el-popover
+      ref="popover1"
+      placement="top-start"
+      title="标题"
+      width="200"
+      trigger="hover"
+      content="这是一段内容,这是一段内容,这是一段内容,这是一段内容。">
+    </el-popover>
+
+    <el-popover
+      ref="popover2"
+      placement="bottom"
+      title="标题"
+      width="200"
+      trigger="click"
+      content="这是一段内容,这是一段内容,这是一段内容,这是一段内容。">
+    </el-popover>
+
+    <el-button v-popover:popover1>hover 激活</el-button>
+    <el-button v-popover:popover2>click 激活</el-button>
+    <el-popover
+      placement="right"
+      title="标题"
+      width="200"
+      trigger="focus"
+      content="这是一段内容,这是一段内容,这是一段内容,这是一段内容。">
+      <el-button slot="reference">focus 激活</el-button>
+    </el-popover>
+  </div>
+
+  <h3>线形进度条 — 百分比外显</h3>
+  <p>用于展示操作进度，告知用户当前状态和预期。</p>
+  <div style="display: flex;">
+    <div style="width:40%">
+      <el-progress :percentage="0"></el-progress>
+      <el-progress :percentage="70"></el-progress>
+      <el-progress :percentage="100" status="success"></el-progress>
+      <el-progress :percentage="50" status="exception"></el-progress>
+    </div>
+    <div style="width: 50%; margin-left: 20px;">
+      <el-progress type="circle" :percentage="0"></el-progress>
+      <el-progress type="circle" :percentage="25"></el-progress>
+      <el-progress type="circle" :percentage="100" status="success"></el-progress>
+      <el-progress type="circle" :percentage="50" status="exception"></el-progress>
+    </div>
+  </div>
+  
+  <h3 id="duo-xuan">表格-多选</h3>
   <p>选择多行数据时使用 Checkbox。</p>
   <el-table
     ref="multipleTable"
@@ -40,7 +155,7 @@
     <el-button @click="toggleSelection()">取消选择</el-button>
   </div>
 
-  <h3 id="zhan-kai-hang">展开行</h3>
+  <h3 id="zhan-kai-hang">表格-展开行</h3>
   <p>当行内容过多并且不想显示横向滚动条时，可以使用 Table 展开行功能。</p>
   <el-table
     :data="tableData5"
@@ -85,6 +200,43 @@
       prop="desc">
     </el-table-column>
   </el-table>
+
+  
+
+  <h3>Collapse 折叠面板</h3>  
+  <p>通过折叠面板收纳内容区域</p>
+  <div>
+    <el-collapse v-model="activeNames" @change="handleChange">
+      <el-collapse-item title="一致性 Consistency" name="1">
+        <div>与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；</div>
+        <div>在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。</div>
+      </el-collapse-item>
+      <el-collapse-item title="反馈 Feedback" name="2">
+        <div>控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；</div>
+        <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
+      </el-collapse-item>
+      <el-collapse-item title="效率 Efficiency" name="3">
+        <div>简化流程：设计简洁直观的操作流程；</div>
+        <div>清晰明确：语言表达清晰且表意明确，让用户快速理解进而作出决策；</div>
+        <div>帮助用户识别：界面简单直白，让用户快速识别而非回忆，减少用户记忆负担。</div>
+      </el-collapse-item>
+      <el-collapse-item title="可控 Controllability" name="4">
+        <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
+        <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
+      </el-collapse-item>
+    </el-collapse>
+  </div>
+
+  <h3>走马灯</h3>
+  <p>可以将指示器的显示位置设置在容器外部</p>
+  <div style="height:300px;" class="demo-carousel">
+    <el-carousel :interval="4000" type="card" height="200px">
+      <el-carousel-item v-for="item in 6" :key="item">
+        <h3>{{ item }}</h3>
+      </el-carousel-item>
+    </el-carousel>
+  </div>
+  
 </div>
 </template>
 
@@ -92,6 +244,7 @@
   export default {
     data() {
       return {
+        activeNames: ['1'],
         tableData3: [{
           date: '2016-05-03',
           name: '王小虎',
@@ -170,11 +323,57 @@
       },
       handleSelectionChange(val) {
         this.multipleSelection = val;
+      },
+      open3() {
+        this.$notify({
+          title: '成功',
+          message: '这是一条成功的提示消息',
+          type: 'success'
+        });
+      },
+
+      open4() {
+        this.$notify({
+          title: '警告',
+          message: '这是一条警告的提示消息',
+          type: 'warning'
+        });
+      },
+
+      open5() {
+        this.$notify.info({
+          title: '消息',
+          message: '这是一条消息的提示消息'
+        });
+      },
+
+      open6() {
+        this.$notify.error({
+          title: '错误',
+          message: '这是一条错误的提示消息'
+        });
       }
     }
   }
 </script>
-<style>
+<style scoped>
+  .item {
+    margin-top: 10px;
+    margin-right: 40px;
+  }
+  h3{
+    font-size: 22px;
+    font-weight: 400;
+    color: #1f2f3d;
+  }
+  p{
+    font-size: 14px;
+    color: #5e6d82;
+    line-height: 1.5em;
+  }
+  .demo-alert .el-alert{
+    margin: 20px 0 0;
+  }
   .demo-table-expand {
     font-size: 0;
   }
@@ -186,5 +385,24 @@
     margin-right: 0;
     margin-bottom: 0;
     width: 50%;
+  }
+  .demo-carousel h3{
+
+  }
+  .el-carousel__item h3 {
+    color: #475669;
+    font-size: 14px;
+    opacity: 0.75;
+    line-height: 200px;
+    margin: 0;
+    text-align: center;
+  }
+  
+  .el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+  }
+  
+  .el-carousel__item:nth-child(2n+1) {
+    background-color: #d3dce6;
   }
 </style>
